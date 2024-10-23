5134222221e7c7cd9367725e8c85b0143223a6be
Astro064 <thomas135969@brisbanegrammar.com>
2024-10-23 15:41:44 +1000

    Gradle Dependencies Updated
    Limelight Code - Not Tested because of Hardware issues
    
    Gradle Dependencies Fixed
    
    Other programming pages added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode_LinearV3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode_LinearV3.java
@@ -1,128 +1,126 @@
-package org.firstinspires.ftc.teamcode;
-
-
-import android.util.Size;
-
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
-
-import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.vision.VisionPortal;
-import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
-import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
-import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
-import org.firstinspires.ftc.vision.tfod.TfodProcessor;
-
-import java.util.List;
-
-
-
-/*
- * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
- * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
- * of the FTC Driver Station. When a selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
- * It includes all the skeletal structure that all linear OpModes contain.
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
- */
-
-//WORKING APRIL TAG CODE WITH TELEMETRY AND DISTANCES - NOT ADJUSTED - TESTED ONLY WITH DIGITAL APRIL TAGS
-
-@Autonomous(name = "Basic: Linear OpMode V5", group = "Linear OpMode")
-public class BasicOpMode_LinearV3 extends LinearOpMode {
-
-    // Declare OpMode members.
-    private ElapsedTime runtime = new ElapsedTime();
-
-
-    AprilTagProcessor myAprilTagProcessor;
-
-
-
-    @Override
-    public void runOpMode() {
-
-
-
-        WebcamName webcam = hardwareMap.get(WebcamName.class, "Webcam 1");
-
-        AprilTag.InitAprilTag(webcam);
-
-
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
-
-        // Initialize the hardware variables. Note that the strings used here as parameters
-        // to 'get' must correspond to the names assigned during the robot configuration
-        // step (using the FTC Robot Controller app on the phone).
-
-
-        // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
-        // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
-        // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
-
-        // Wait for the game to start (driver presses PLAY)
-        waitForStart();
-        runtime.reset();
-
-        // run until the end of the match (driver presses STOP)
-        while (opModeIsActive()) {
-
-            AprilTag.telemetryAprilTag(telemetry);
-
-
-
-
-            // Setup a variable for each drive wheel to save power level for telemetry
-            double leftPower;
-            double rightPower;
-
-
-
-            // Choose to drive using either Tank Mode, or POV Mode
-            // Comment out the method that's not used.  The default below is POV.
-
-            // POV Mode uses left stick to go forward, and right stick to turn.
-            // - This uses basic math to combine motions and is easier to drive straight.
-            double drive = -gamepad1.left_stick_y;
-            double turn = gamepad1.right_stick_x;
-
-
-                leftPower = Range.clip(drive + turn, -1.0, 1.0);
-                rightPower = Range.clip(drive - turn, -1.0, 1.0);
-
-                // Tank Mode uses one stick to control each wheel.
-                // - This requires no math, but it is hard to drive forward slowly and keep straight.
-                // leftPower  = -gamepad1.left_stick_y ;
-                // rightPower = -gamepad1.right_stick_y ;
-
-                // Send calculated power to wheels
-
-
-                // Show the elapsed game time and wheel power.
-
-
-
-
-
-
-
-                AprilTag.UpdateAprilTag();
-                AprilTag.telemetryAprilTag(telemetry);
-                telemetry.addData("Status", "Run Time: " + runtime.toString());
-                telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
-                telemetry.update();
-
-               // LocalisationController.LocalisationController(webcam, telemetry);
-
-            }
-        }
-    }
-
+//package org.firstinspires.ftc.teamcode;
+//
+//
+//import android.util.Size;
+//
+//import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+//import com.qualcomm.robotcore.util.ElapsedTime;
+//import com.qualcomm.robotcore.util.Range;
+//
+//import org.firstinspires.ftc.robotcore.external.Telemetry;
+//import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+//import org.firstinspires.ftc.vision.VisionPortal;
+//import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+//import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
+//import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+//import java.util.List;
+//
+//
+//
+///*
+// * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+// * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+// * of the FTC Driver Station. When a selection is made from the menu, the corresponding OpMode
+// * class is instantiated on the Robot Controller and executed.
+// *
+// * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+// * It includes all the skeletal structure that all linear OpModes contain.
+// *
+// * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+// * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
+// */
+//
+////WORKING APRIL TAG CODE WITH TELEMETRY AND DISTANCES - NOT ADJUSTED - TESTED ONLY WITH DIGITAL APRIL TAGS
+//
+//@Autonomous(name = "Basic: Linear OpMode V5", group = "Linear OpMode")
+//public class BasicOpMode_LinearV3 extends LinearOpMode {
+//
+//    // Declare OpMode members.
+//    private ElapsedTime runtime = new ElapsedTime();
+//
+//
+//    AprilTagProcessor myAprilTagProcessor;
+//
+//
+//
+//    @Override
+//    public void runOpMode() {
+//
+//
+//
+//        WebcamName webcam = hardwareMap.get(WebcamName.class, "Webcam 1");
+//
+////        AprilTag.InitAprilTag(webcam);
+//
+//
+//        telemetry.addData("Status", "Initialized");
+//        telemetry.update();
+//
+//        // Initialize the hardware variables. Note that the strings used here as parameters
+//        // to 'get' must correspond to the names assigned during the robot configuration
+//        // step (using the FTC Robot Controller app on the phone).
+//
+//
+//        // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
+//        // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
+//        // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
+//
+//        // Wait for the game to start (driver presses PLAY)
+//        waitForStart();
+//        runtime.reset();
+//
+//        // run until the end of the match (driver presses STOP)
+//        while (opModeIsActive()) {
+//
+//            AprilTag.telemetryAprilTag(telemetry);
+//
+//
+//
+//
+//            // Setup a variable for each drive wheel to save power level for telemetry
+//            double leftPower;
+//            double rightPower;
+//
+//
+//
+//            // Choose to drive using either Tank Mode, or POV Mode
+//            // Comment out the method that's not used.  The default below is POV.
+//
+//            // POV Mode uses left stick to go forward, and right stick to turn.
+//            // - This uses basic math to combine motions and is easier to drive straight.
+//            double drive = -gamepad1.left_stick_y;
+//            double turn = gamepad1.right_stick_x;
+//
+//
+//                leftPower = Range.clip(drive + turn, -1.0, 1.0);
+//                rightPower = Range.clip(drive - turn, -1.0, 1.0);
+//
+//                // Tank Mode uses one stick to control each wheel.
+//                // - This requires no math, but it is hard to drive forward slowly and keep straight.
+//                // leftPower  = -gamepad1.left_stick_y ;
+//                // rightPower = -gamepad1.right_stick_y ;
+//
+//                // Send calculated power to wheels
+//
+//
+//                // Show the elapsed game time and wheel power.
+//
+//
+//
+//
+//
+//
+//
+//                AprilTag.UpdateAprilTag();
+//                AprilTag.telemetryAprilTag(telemetry);
+//                telemetry.addData("Status", "Run Time: " + runtime.toString());
+//                telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+//                telemetry.update();
+//
+//               // LocalisationController.LocalisationController(webcam, telemetry);
+//
+//            }
+//        }
+//    }
+//
